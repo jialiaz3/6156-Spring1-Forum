6c5a6f17100a2a7f48390d433b1138ac9dd8c487
Wenye Ma <mwysjtu@gmail.com>
2021-10-13 14:39:50 -0400

    app.py update

--- app.py
+++ app.py
@@ -20,25 +20,48 @@ def hello_world():
     return '<u>Hello World!</u>'
 
 
-@app.route('/imdb/artists/<prefix>')
-def get_artists_by_prefix(prefix):
-    res = IMDBArtistResource.get_by_name_prefix(prefix)
-    rsp = Response(json.dumps(res), status=200, content_type="application/json")
-    return rsp
 
 
-@app.route('/users')
-def get_users():
-    res = UserResource.get_by_template(None)
-    rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
-    return rsp
+# /forum
+@app.route('/forum', methods=['GET', 'POST'])
+def get_forum():
+    if request.method == 'GET':
+        res = UserResource.find_by_template(None)
+        rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
+        return rsp
+    elif request.method == 'POST':
+        f_id = request.form['f_id']
+        title = request.form['title']
+        content = request.form['content']
+        username = request.form['username']
 
+        create_data = {"f_id": f_id, "title": title, "content": content, "username": username}
+        res = UserResource.create(create_data)
+        rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
+        return rsp
 
-@app.route('/<db_schema>/<table_name>/<column_name>/<prefix>')
-def get_by_prefix(db_schema, table_name, column_name, prefix):
-    res = RDBService.get_by_prefix(db_schema, table_name, column_name, prefix)
-    rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
-    return rsp
+# /forum/<f_id>
+@app.route('/forum/<f_id>', methods=['GET', 'DELETE'])
+def get_forum_by_f_id(f_id):
+    if request.method == 'GET':
+        template = {"f_id":f_id}
+        res = UserResource.find_by_template(template)
+        rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
+        return rsp
+    elif request.method == 'DELETE':
+        template = {"f_id": f_id}
+        res = UserResource.delete(template)
+        rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
+        return rsp
+
+# /forum/<title>
+@app.route('/forum/<title>', methods=['GET'])
+def get_forum_by_title(title):
+    if request.method == 'GET':
+        template = {"title":title}
+        res = UserResource.find_by_template(template)
+        rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
+        return rsp
 
 
 if __name__ == '__main__':
