45d8a818da62af7aa438cf55eb8863d75102459d
stellajc <44332454+stellajc@users.noreply.github.com>
2021-11-29 16:44:34 -0500

    update post code

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_forums():
         else:
             create_data = request.json
         res = ForumResource.create(create_data)
-        rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
+        rsp = Response(json.dumps(res, default=str), status=201, content_type="application/json")
         return rsp
 
 
